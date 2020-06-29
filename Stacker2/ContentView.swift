dc9ee159406b29e9e863fad80fb934d3d7752439
Paul Barnard <barnard@ygomi.com>
2020-06-29 13:04:02 +0100

    Initial Commit

---
+++ Stacker2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Stacker2
+//
+//  Created by Paul Barnard on 29/06/2020.
+//  Copyright © 2020 Paul Barnard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
